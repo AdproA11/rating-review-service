f83520e999325c2b98fc9314535e248f8b98c49f
adhan-857 <adhan.andika857@gmail.com>
2024-05-25 21:14:28 +0700

    Making the final version model for Rating & Review

--- src/main/java/id/ac/ui/cs/advprog/ratingreviewservice/model/RatingReview.java
+++ src/main/java/id/ac/ui/cs/advprog/ratingreviewservice/model/RatingReview.java
@@ -1,5 +1,19 @@
 package id.ac.ui.cs.advprog.ratingreviewservice.model;
 
-public class RatingReview {
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@Getter @Setter
+public class RatingReview {
+    private long subscriptionId;
+    private String ratingReviewId;
+    private String ownerName;
+    private int rating;
+    private String review;
+
+    public RatingReview() {
+        this.ratingReviewId = UUID.randomUUID().toString();
+    }
 }
